f02edec26dd42199439c0f86b00c9075caf69074
Juan Miguel <oliveirajuan243@gmail.com>
2024-01-02 14:36:37 -0300

    checagem de cep no segundo setp concluido com sucesso

--- src/screens/recebimento/index.jsx
+++ src/screens/recebimento/index.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import "../../App.css"
 import HeaderPostaAqui from "../../components/header";
 import { cpfMask, phoneMask } from "../../components/mask";
+import { useForm } from "react-hook-form";
 
 const InputsReceiver = () =>{
     const [valorCpf, setValorCpf] = useState(''); // constantes para o input cpf
     const [phoneValor, setPhoneValor] = useState('')
+    const {register, setValue, focus} = useForm();
     {/*
     const[nome,setNome] = useState();
     const [email,SetEmail] = useState();
@@ -39,6 +42,19 @@ const InputsReceiver = () =>{
         const {value}=event.target;
         setValorCpf(cpfMask(value));
     }
+    const CheckCep =(e)=>{
+        const cep = e.target.value.replace(/\D/g,'');
+        console.log(cep);
+        fetch(`https://viacep.com.br/ws/${cep}/json/`).then(res=>res. json()).then(data=>{
+            console.log(data);
+            setValue('address',data.logradouro);
+            setValue('neighbirhood',data.bairro);
+            setValue('city',data.localidade);
+            setValue('uf',data.uf);
+            focus('addressNumber')
+            
+        });
+    }
     return(
         <>
             <HeaderPostaAqui />
@@ -46,25 +62,64 @@ const InputsReceiver = () =>{
                 <div className="title">
                     <h2>Daods</h2>
                 </div>
+                <form action="">
+                    <div className="InputsdePreenchimento">
+                        <label htmlFor="">
+                            Nome Completo <br />
+                            <input type="text" name="" id="" />
+                        </label>
+                        <label htmlFor="">
+                            CPF <br />
+                            <input type="text" onChange={CampoCpf} value={valorCpf} name="" id="" />
+                        </label>
+                        <label htmlFor="">
+                            Telefone <br />
+                            <input type="text" onChange={CampoPhone} value={phoneValor} name="" id="" />
+                        </label>
+                        <label htmlFor="">
+                            email <br />
+                            <input type="text" name="" id="" />
+                        </label>
+                    </div>
+                    
+                    <div className="InputsPreenchimento">
+                        <label htmlFor="">
+                            Cep 
+                            <input {...register("cep")} onChange={CheckCep} type="text" />
+                        </label>
+                        <label htmlFor="">
+                            Estado 
+                            <input {...register("uf")} type="text" />
+                        </label>
+                        <label htmlFor="">
+                            cidade 
+                            <input {...register("city")} type="text" />
+                        </label>
+                        <label htmlFor="">
+                            bairro
+                            <input {...register("neighbothood")} type="text" />
+                        </label>
+                        <label htmlFor="">
+                            rua 
+                            <input type="text" name="" id="" />
+                        </label>
+                        
+                    </div>
+                    <div className="InputsPreenchimento">
+                        <label htmlFor="">
+                            Numero
+                            <input type="text" {...register("addressNumber")} />
+                        </label>
+                        <label htmlFor="">
+                            Complemento
+                            <input type="text" />
+                        </label>
+                        
+                    </div>
+                    
 
-                <div className="InputsdePreenchimento">
-                    <label htmlFor="">
-                        Nome Completo <br />
-                        <input type="text" name="" id="" />
-                    </label>
-                    <label htmlFor="">
-                        CPF <br />
-                        <input type="text" onChange={CampoCpf} value={valorCpf} name="" id="" />
-                    </label>
-                    <label htmlFor="">
-                        Telefone <br />
-                        <input type="text" onChange={CampoPhone} value={phoneValor} name="" id="" />
-                    </label>
-                    <label htmlFor="">
-                        email <br />
-                        <input type="text" name="" id="" />
-                    </label>
-                </div>
+                </form>
+
             </div>
          
         </>
