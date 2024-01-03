09c2bf3af88ce601083e53e79048674ccf1f34e4
Juan Miguel <oliveirajuan243@gmail.com>
2024-01-03 13:20:09 -0300

    estilo do step3

--- src/components/forms/formPackage/index.jsx
+++ src/components/forms/formPackage/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../../../index.css"
 
 function FormularioPacote({name,sender,receiver}){
     return(
@@ -7,7 +8,7 @@ function FormularioPacote({name,sender,receiver}){
                 <h2>{name}</h2>
             </div>
             <div className="InputsdePreenchimento">
-                <div className="items-center vertical">
+                <div className="  items-center vertical">
                     <label htmlFor="">Peso <br />
                         <input type="text" />
                     </label>
@@ -26,16 +27,54 @@ function FormularioPacote({name,sender,receiver}){
 
             </div>
             <div className="InputsdePreenchimento">
-                <div className="items-center vertical">
-                    <label htmlFor="">Logistica Reversa
-                        <input type="checkbox" name="logistica Reversa" id="" />
-                    </label>
-                    <label htmlFor="">Logistica Reversa
-                        <input type="checkbox" name="logistica Reversa" id="" />
-                    </label>
-                    <label htmlFor="">Logistica Reversa
-                        <input type="checkbox" name="logistica Reversa" id="" />
-                    </label>
+                
+                <div className=" items-center vertical">
+                    
+                    <div className="formInput items-center">
+                        <label htmlFor="">Logistica Reversa</label>
+                        <input type="checkbox" className="checkInput" />
+
+                    </div>
+                    <div className="formInput items-center">
+                        <label htmlFor="">Aviso de Recebimento</label>
+                        <input className="checkInput" type="checkbox" />
+
+                    </div>
+                    <div className="formInput items-center">
+                        <label htmlFor="">Mãos próprias</label>
+                        <input type="checkbox" />
+
+                    </div>
+
+                </div>
+            </div>
+            <div className="InputsdePreenchimento ">
+                <div className="flex widthTotal">
+                    <div className="flexBox items-center vertical">
+                        <div className=" horizontal">
+                            <div className=" formInput" >
+                                <label htmlFor="">
+                                <input type="text" />
+
+                                </label>
+
+                            </div>
+                            <div className="formInput">
+
+                                <label htmlFor="">
+
+                                <input type="text" name="" id="" />
+                                </label>
+                            </div>
+
+                        </div>
+                        <div className="flexBox widthTotal">
+                            <div className="formInput">
+                                <input type="text" className="descricao" />
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
